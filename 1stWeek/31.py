eb9b0de2dd18fb885b1c40c748d43545d37ecf7e
leezu2020 <ejh7406@naver.com>
2023-02-01 22:45:05 +0900

    [1주차] comment: 줄바꿈

--- 1stWeek/31.py
+++ 1stWeek/31.py
@@ -3,6 +3,8 @@ import random
 print('베스킨라빈스 31 게임 프로그램')
 
 # 유효성 범위(valid_list) 내에서 입력 받기
+
+
 def validate_input(prompt, valid_list):
     while True:
         value = input(prompt)
@@ -14,6 +16,8 @@ def validate_input(prompt, valid_list):
     return value
 
 # 컴퓨터가 이기는 숫자 반환
+
+
 def call_computer(call_status):
     if call_status % 4 == 0:
         computer_call = 2
@@ -23,8 +27,9 @@ def call_computer(call_status):
         computer_call = 3
     else:
         computer_call = random.randint(1, 3)
-        
+
     return computer_call
+
 
 def call_numbers(size, called):
     for _ in range(size):
@@ -35,6 +40,7 @@ def call_numbers(size, called):
             break
 
     return called
+
 
 # 게임 시작
 order = validate_input('순서를 입력하세요. (선공 1, 후공 0 입력) : ', ['0', '1'])
@@ -52,11 +58,11 @@ while call < 31:
     else:
         print('컴퓨터 차례')
         size_of_call = call_computer(call)
-        
+
         call = call_numbers(size_of_call, call)
-            
+
     count += 1
-        
+
 if count % 2 == order:
     print("사용자의 승리!")
 else:
